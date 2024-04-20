0cb9886cfd9498896cdd842f6903bf69ac3f424d
Semal Patel <semalPatel@users.noreply.github.com>
2024-04-20 14:56:24 -0700

    remove explicit docker-compose installation since the docker script already includes that

--- docker/install_docker_pi.sh
+++ docker/install_docker_pi.sh
@@ -9,21 +9,11 @@ echo "Removing images, containers, and volumes"
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
 
-# install the latest version of docker
+sudo apt-get update
+
+# install the latest version of docker, includes docker-compose
 echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo bash get-docker.sh
-
-# optionally ask for docker-compose installation
-read -p 'Install docker-compose? [y/n] ' response
-if [[ "$response" == "y" || "$response" == "Y" || "$response" == "yes" ]]; then
-echo "Installing docker-compose"
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
-sudo apt-get install -y python3 python3-pip
-sudo pip3 install docker-compose
-else
-echo "Skipping docker-compose"
-fi
 
 echo "Done with docker installation"
