d44f5f50b2bbb2fa6ce085fea38e70e800efd70d
Semal Patel <semalPatel@users.noreply.github.com>
2024-04-19 19:30:13 -0700

    remove the apt-get way to install docker, use the docker script directly

--- docker/install_docker_pi.sh
+++ docker/install_docker_pi.sh
@@ -9,32 +9,19 @@ echo "Removing images, containers, and volumes"
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
 
-# add docker's official GPG key:
-echo "Adding docker's official GPG key"
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# set up docker's APT repository:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
 # install the latest version of docker
 echo "Installing docker"
-sudo apt-get install docker.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
 # optionally ask for docker-compose installation
 read -p 'Install docker-compose? [y/n] ' response
 if [[ "$response" == "y" || "$response" == "Y" || "$response" == "yes" ]]; then
 echo "Installing docker-compose"
-sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-echo "Setting executable permissions for docker-compose"
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
 else
 echo "Skipping docker-compose"
 fi
